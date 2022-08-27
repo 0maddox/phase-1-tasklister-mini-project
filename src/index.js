df9fe34d01c32f853e2a122cb0c4496d129052a8
nicholas <nickkiim7@gmail.com>
2022-08-27 19:05:34 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form =document.querySelector("form")
+  form.addEventListener("sumbit", (e)=>{
+    taskBuild(e.target.new-task-description.value)
+  e.preventDefault()
+    form.reset()
+  })
+  
+  });
+
+  function taskBuild(doing){
+    let p = document.createElement("p")
+    let btn = document.createElement("button")
+    btn.addEventListener("click", doDelete)
+    btn.textContent = "M"
+    p.textContent = `${doing}`
+    p.appendChild(btn)
+    console.log(p)
+    document.querySelector(`#container`).appendChild(p)
+  }
+  function toogleDelete (e) {
+    e.target.parentNode.remove()
+  }
